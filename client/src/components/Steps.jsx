34bc6bbb53a70b31ef07deec56ffe09c2e88eca9
DurgeshSChauhan007 <durgeshsinghchauhan97.4@gmail.com>
2025-03-26 17:43:09 +0530

    Webhooks Added

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -8,7 +8,7 @@ const Steps = () => {
         text-transparent'>Steps to remove background <br /> image in seconds</h1>
         <div className='flex items-start flex-wrap gap-4 mt-16 xl:mt-24 justify-center '>
 
-            <div className='flex items-start gap-4 bg-white drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500'>
+            <div className='flex items-start gap-4 bg-white drop-shadow-md p-6 pb-10 rounded hover:scale-105 transition-all duration-500'>
                 <img className='max-w-9' src={assets.upload_icon} alt="" />
                 <div>
                     <p className='text-xl font-medium'>Upload image</p>
