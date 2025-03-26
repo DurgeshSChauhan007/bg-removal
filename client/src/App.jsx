a4a1f076809bd94b9e9d35d5977b88cf199b16d9
DurgeshSChauhan007 <durgeshsinghchauhan97.4@gmail.com>
2025-03-26 23:07:29 +0530

    commit Project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Home from './pages/Home'
 import BuyCredit from './pages/BuyCredit'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   return (
     <div className='min-h-screen bg-slate-50'>
+      <ToastContainer position='bottom-right'/>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
